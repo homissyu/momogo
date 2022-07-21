cb146d2b82e4259aa4f6153eb6eb088976ea6de1
homissyu <homissyu@gmail.com>
2022-07-21 16:13:53 +0900

    5th commit

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-test
+const express = require('express');
+const asyncify = require('express-asyncify');
+const cors = require('cors');
+ 
+const app = asyncify(express());
+
+const compression = require('compression');
+app.use(compression());
+
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const logger = require('./utils/logger');
+
+// CORS 설정
+app.use(cors());
+
+
+// var app = express();
+var PORT= process.env.PORT || 3000;
+
+// Publishing Version
+const PUB_VER = "";
+
+// https redirect
+
+app.all('*', (req, res, next) => { 
+    let userAgent = req.headers['User-Agent'] || req.userAgent; 
+    let protocol = req.headers['x-forwarded-proto'] || req.protocol; 
+    // console.log(`User-Agent:${userAgent}`);
+    if (req.hostname == 'localhost' || protocol == 'https') { 
+        next(); 
+    } 
+});
+
+//urlEncode
+app.use(bodyParser.urlencoded({extended : true}));
+
+//view
+app.set('views', __dirname+PUB_VER+'/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+PUB_VER+"/public"));
+
+//use cache-control
+var   maxAge =  60 * 60 * 24;
+app.all('/img/*',function(req,res,next){
+    res.set('Cache-Control', 'public, max-age=31557600');
+    next();
+});
+app.use("/", require(__dirname+PUB_VER+"/server/index"));
+
+//listen
+app.listen(PORT, function(){
+    logger.info(`Listening on port ${PORT}...`);
+});
+
+app.use(function(err, req, res, next) { 
+    logger.error(err);
+});
